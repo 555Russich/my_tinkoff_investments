daea69c6545bf651d268dd022a625eaf39eb16a2
Dmitriy Kutsokon <Kutsokondima@yandex.ru>
2024-05-09 13:06:13 +0300

    isinstance Candle fix + clearing

--- tests/api_calls/test_market_data.py
+++ tests/api_calls/test_market_data.py
@@ -12,21 +12,9 @@ from tests.dataset import SBER, dataset_candles
 
 @pytest.mark.parametrize("instrument,case", dataset_candles)
 async def test_get_candles(instrument, case) -> None:
-    # from datetime import datetime, timezone
-    # from tinkoff.invest import CandleInterval
-    # from_ = datetime(year=2023, month=10, day=25, hour=20, minute=30, tzinfo=timezone.utc)
-    # to = datetime(year=2023, month=10, day=26, hour=7, minute=30, tzinfo=timezone.utc)
-    # r = await get_candles(instrument_id='974077c4-d893-4058-9314-8f1b64a444b8', from_=from_, to=to, interval=CandleInterval.CANDLE_INTERVAL_1_MIN)
-    # for c in r:
-    #     print(c)
-    # exit()
-
     r = await get_candles(instrument_id=instrument.uid, from_=case.dt_from, to=case.dt_to, interval=case.interval)
     assert isinstance(r, Candles)
     assert isinstance(r[0], Candle)
-    # print(len(r))
-    # print(f'{r[0].time=}')
-    # print(f'{r[-1].time=}')
     assert len(r) == case.count_candles
     assert r[0].time == case.dt_first_candle
     assert r[-1].time == case.dt_last_candle
